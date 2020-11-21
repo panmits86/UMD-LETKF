272c1f0561a9278f068e8c9e0ef58f3c52cfafe8
Stylianos Flampouris <stylianos.flampouris@noaa.gov>
2020-11-20 22:46:39 -0500

    Add the installation of ncview and ncdump

--- dep/setup_libraries.sh
+++ dep/setup_libraries.sh
@@ -5,4 +5,5 @@ sudo apt install gfortran gcc g++
 sudo apt install openmpi-bin 
 sudo apt install libnetcdf-dev libnetcdff-dev
 sudo apt install libblas-dev 
-sudo apt install liblapack-dev 
+sudo apt install liblapack-dev
+sudo apt install ncview netcdf-bin
