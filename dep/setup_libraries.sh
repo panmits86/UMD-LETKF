6328208f601c56b4964eefbfc4988bf6bac2bfb7
Stylianos Flampouris <stylianos.flampouris@noaa.gov>
2020-11-20 22:36:42 -0500

    Adding the automated installation of cmakei gfortran gcc g++ openmpi-bin libnetcdf-dev libnetcdff-dev libblas-dev liblapack-dev

---
+++ dep/setup_libraries.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install cmake
+sudo apt install gfortran gcc g++
+sudo apt install openmpi-bin 
+sudo apt install libnetcdf-dev libnetcdff-dev
+sudo apt install libblas-dev 
+sudo apt install liblapack-dev 
